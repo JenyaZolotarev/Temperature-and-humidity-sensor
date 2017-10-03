a8616d509e6fd07759d2fafe68148fffb2719be7
Jenya Zolotarev <yevheniyzolotarev@gmail.com>
2017-10-03 16:57:40 +0300

    My first commit

---
+++ DTH22/Inc/time_control.h
@@ -0,0 +1,15 @@
+/*
+ * time_control.h
+ *
+ *  Created on: 1 вер. 2017 р.
+ *      Author: Jenya
+ */
+
+#ifndef TIME_CONTROL_H_
+#define TIME_CONTROL_H_
+
+uint32_t getUs(void);
+void delayUs(uint16_t micros);
+
+
+#endif /* TIME_CONTROL_H_ */
